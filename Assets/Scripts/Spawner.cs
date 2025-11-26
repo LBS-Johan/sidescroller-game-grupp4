54c4e5633e7eae7be7a7dbd287e8c4ae2efab3f5
PontanVIKgit <pontus.piiparinenclaesson@elev.ga.lbs.se>
2025-11-26 13:13:41 +0100

    det funkar typ bra nu

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
@@ -19,12 +17,12 @@ public class Spawner : MonoBehaviour
     Transform spawnLocation;
 
 
-    float timer = 0;
+    public float timer = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -35,7 +33,7 @@ public class Spawner : MonoBehaviour
         if ((fireButton == KeyCode.None || Input.GetKeyDown(fireButton)) && timer <= 0)
         {
             Vector2 pos = transform.position;
-            if(spawnLocation != null)
+            if (spawnLocation != null)
             {
                 pos = spawnLocation.position;
             }
@@ -47,7 +45,7 @@ public class Spawner : MonoBehaviour
             {
                 rb.linearVelocity = fireDirection;
             }
-            
+
 
             timer = coolDown;
         }
